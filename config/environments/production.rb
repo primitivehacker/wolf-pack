3a464286565f871074be37795f232ab6615d25d3
Derek Johnson <siestaderek@gmail.com>
2017-10-05 05:58:37 +0000

    Changed Heroku email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'wolf-pack.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'paktola.herokuapp.com', :protocol => 'https'}
 end
